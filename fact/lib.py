67a877e12adda7fc8dff4f45980a1dbbda2a17fe
johnthagen <johnthagen@gmail.com>
2018-11-10 19:02:46 -0500

    Fix docstring formatting

--- fact/lib.py
+++ fact/lib.py
@@ -1,5 +1,5 @@
 def factorial(n: int) -> int:
-    """Computes the factorial of `n` through a recursive algorithm."""
+    """Computes the factorial of ``n`` through a recursive algorithm."""
     if n == 0:
         return 1
 
