cb8b9c21d57c5527ce571e6fe23b47c1b20ebfec
johnthagen <johnthagen@gmail.com>
2020-08-31 13:37:28 -0400

    Improve black/isort configuration and usage

--- tests/test_fact.py
+++ tests/test_fact.py
@@ -3,24 +3,26 @@ import pytest
 from fact.lib import InvalidFactorialError, factorial
 
 
-# fmt: off
-@pytest.mark.parametrize('n,expected', [
-    (1, 1),
-    (2, 2),
-    (3, 6),
-    (10, 3628800),
-])
-# fmt: on
+@pytest.mark.parametrize(
+    "n,expected",
+    [
+        (1, 1),
+        (2, 2),
+        (3, 6),
+        (10, 3628800),
+    ],
+)
 def test_factorial(n: int, expected: int) -> None:
     assert factorial(n) == expected
 
 
-# fmt: off
-@pytest.mark.parametrize('n', [
-    (-1),
-    (-100),
-])
-# fmt: on
+@pytest.mark.parametrize(
+    "n",
+    [
+        (-1),
+        (-100),
+    ],
+)
 def test_invalid_factorial(n: int) -> None:
     with pytest.raises(InvalidFactorialError):
         factorial(n)
