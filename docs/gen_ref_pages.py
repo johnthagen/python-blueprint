37bc839691186d0fb935cac40838bbb784384b6c
johnthagen <johnthagen@gmail.com>
2022-04-10 07:55:09 -0400

    Simplify as_posix in code reference generation

--- docs/gen_ref_pages.py
+++ docs/gen_ref_pages.py
@@ -3,7 +3,7 @@
 Source: https://mkdocstrings.github.io/recipes/#automatic-code-reference-pages
 """
 
-from pathlib import Path, PurePosixPath
+from pathlib import Path
 
 import mkdocs_gen_files
 
@@ -21,7 +21,7 @@ for path in sorted(Path("src").rglob("*.py")):
     elif parts[-1] == "__main__":
         continue
 
-    nav[parts] = str(PurePosixPath(doc_path))
+    nav[parts] = str(doc_path.as_posix())
 
     with mkdocs_gen_files.open(full_doc_path, "w") as fd:
         ident = ".".join(parts)
