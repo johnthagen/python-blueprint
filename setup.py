d43fb96b91808371fce3ff249e62b666641ae974
johnthagen <johnthagen@users.noreply.github.com>
2022-05-05 08:10:00 -0400

    Update trove classifiers

--- setup.py
+++ setup.py
@@ -40,9 +40,10 @@ setuptools.setup(
     zip_safe=False,
     license="MIT",
     classifiers=[
-        "Development Status :: 4 - Beta",
+        "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3 :: Only",
