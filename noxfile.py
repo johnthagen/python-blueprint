b5e82d379b033b50432c3faef05c5353fcb10ae5
johnthagen <johnthagen@gmail.com>
2022-05-15 08:49:25 -0400

    Reuse the existing poetry environment for simple project scripts

--- noxfile.py
+++ noxfile.py
@@ -4,12 +4,10 @@ from nox_poetry import Session, session
 nox.options.reuse_existing_virtualenvs = True
 nox.options.sessions = ["fmt_check", "type_check", "lint", "test", "docs"]
 
-test_dependencies = ["pytest", "pytest-cov"]
-
 
 @session(python=["3.8", "3.9", "3.10"])
 def test(s: Session) -> None:
-    s.install(".", *test_dependencies)
+    s.install(".", "pytest", "pytest-cov")
     s.run(
         "python",
         "-m",
@@ -24,72 +22,49 @@ def test(s: Session) -> None:
     )
 
 
-@session
+# For some sessions, set venv_backend="none" to simply execute scripts using the existing
+# environment. This requires that nox is installed and run using `poetry install`.
+@session(venv_backend="none")
 def type_check(s: Session) -> None:
     # It is important to install the main project and test dependencies, as some packages contain
     # inline type hints (PEP 561) that mypy will use.
-    s.install(".", *test_dependencies, "mypy")
     s.run("mypy", "src", "tests")
 
 
-@session
+@session(venv_backend="none")
 def lint(s: Session) -> None:
-    s.install(
-        "flake8",
-        "flake8-bugbear",
-        "flake8-broken-line",
-        "flake8-comprehensions",
-        "pep8-naming",
-        "pyproject-flake8",
-    )
     # Run pyproject-flake8 entrypoint to support reading configuration from pyproject.toml.
     s.run("pflake8")
 
 
-fmt_dependencies = ["isort", "black"]
-
-
-@session
+@session(venv_backend="none")
 def fmt(s: Session) -> None:
-    s.install(*fmt_dependencies)
     s.run("isort", ".")
     s.run("black", ".")
 
 
-@session
+@session(venv_backend="none")
 def fmt_check(s: Session) -> None:
-    s.install(*fmt_dependencies)
     s.run("isort", "--check", ".")
     s.run("black", "--check", ".")
 
-
-doc_dependencies = [
-    "mkdocs-material",
-    "mkdocs-htmlproofer-plugin",
-    "mkdocstrings[python]",
-    "mkdocs-gen-files",
-    "mkdocs-literate-nav",
-]
 
 # Environment variable needed for mkdocstrings-python to locate source files.
 doc_env = {"PYTHONPATH": "src"}
 
 
-@session
+@session(venv_backend="none")
 def docs(s: Session) -> None:
-    s.install(*doc_dependencies)
     s.run("mkdocs", "build", env=doc_env)
 
 
-@session
+@session(venv_backend="none")
 def docs_serve(s: Session) -> None:
-    s.install(*doc_dependencies)
     s.run("mkdocs", "serve", env=doc_env)
 
 
-@session
+@session(venv_backend="none")
 def docs_github_pages(s: Session) -> None:
-    s.install(*doc_dependencies)
     s.run("mkdocs", "gh-deploy", "--force", env=doc_env)
 
 
