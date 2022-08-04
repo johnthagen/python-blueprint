1d180f1581b274fc2a6cc830c34098add2022791
johnthagen <johnthagen@gmail.com>
2022-08-04 08:07:33 -0400

    Enable Typer Rich integration

--- src/fact/cli.py
+++ src/fact/cli.py
@@ -1,26 +1,20 @@
 #!/usr/bin/env python3
 
-import colorama
-import typer
-from typer import Argument
+from rich.console import Console
+from typer import Argument, Typer
 
 from fact.lib import factorial
 
+app = Typer(add_completion=False)
 
+
+@app.command()
 def main(n: int = Argument(..., min=0, help="The input n of fact(n)")) -> None:
     """Compute factorial of a given input."""
-    colorama.init(autoreset=True, strip=False)
 
-    print(
-        f"fact({colorama.Fore.CYAN}{n}{colorama.Fore.RESET}) = "
-        f"{colorama.Fore.GREEN}{factorial(n)}{colorama.Fore.RESET}"
-    )
-
-
-def entry_point() -> None:
-    typer.run(main)
+    Console().print(f"fact({n}) = {factorial(n)}")
 
 
 # Allow the script to be run standalone (useful during development).
 if __name__ == "__main__":
-    entry_point()
+    app()
