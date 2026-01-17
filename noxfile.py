d4fbf756a30275016c1a982f45c672272b4c087b
johnthagen <johnthagen@users.noreply.github.com>
2026-01-17 09:26:57 -0500

    Support free threading (#333)

--- noxfile.py
+++ noxfile.py
@@ -7,7 +7,7 @@ options.sessions = ["lint", "type_check", "test", "docs"]
 
 
 @session(
-    python=["3.10", "3.11", "3.12", "3.13", "3.14"],
+    python=["3.10", "3.11", "3.12", "3.13", "3.14", "3.14t"],
     uv_groups=["test"],
 )
 def test(s: Session) -> None:
